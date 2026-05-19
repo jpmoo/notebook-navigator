bf19539fbca90e1c2cb70d398b189cb5df721983
Johan Sanneblad <johan@sanneblad.se>
2026-05-19 18:37:56 +0200

    Update releaseNotes.ts

--- src/releaseNotes.ts
+++ src/releaseNotes.ts
@@ -81,6 +81,14 @@ export interface ReleaseNote {
  */
 const RELEASE_NOTES: ReleaseNote[] = [
     {
+        version: '3.0.1',
+        date: '2026-05-26',
+        showOnUpdate: true,
+        improved: [
+            '**Manual sort.** **Edit sort order...** now supports keyboard navigation.'
+        ]
+    },
+    {
         version: '3.0.0',
         date: '2026-05-18',
         showOnUpdate: true,
