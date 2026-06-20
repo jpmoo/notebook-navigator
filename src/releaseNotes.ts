53f2f625fed3caba6d3c3f57ee13ed3794693637
Johan Sanneblad <johan@sanneblad.se>
2026-06-20 20:39:58 +0200

    Update releaseNotes.ts

--- src/releaseNotes.ts
+++ src/releaseNotes.ts
@@ -91,8 +91,9 @@ export interface ReleaseNote {
 const RELEASE_NOTES: ReleaseNote[] = [
     {
         version: '3.2.0',
-        date: '2026-06-22',
+        date: '2026-06-21',
         showOnUpdate: true,
+        bannerUrl: true,
         info: '**This release makes Notebook Navigator start MUCH faster!** Most feature code now loads the first time you use a feature instead of while Obsidian starts up, and several background tasks no longer run during plugin load. Many users will see almost a tenfold improvement to startup time.',
         new: [
             '==New icon and color picker!== Redesigned and merged the icon and color pickers into a unified panel with preview, saturation/value rectangle and a new hue slider.',
